d9c81a6fcd0cae5657a53cdebe058e4d32dd0728
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-01-31 23:49:48 +0530

    add per route configurable rate limit

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,32 @@ const rateLimiter = require("./middleware/rateLimiter");
 
 const app = express();
 
-app.use(rateLimiter({ limit: 5, windowMs: 60000 }));
 
-app.get("/", (req, res) => {
+//no limiter as it is global routes
+app.get("/health", (req, res) => { 
     res.json({
+        status:"ok",
         message: "Request Successful"
     });
 });
+
+// strict limiter 
+app.get("/login", rateLimiter({ limit: 3, windowMs: 60000}), 
+    (req, res) => {
+        res.json({
+            message: "login route hit"
+        });
+    }
+);
+
+// relaxed limiter 
+app.get("/search", rateLimiter({ limit: 10, windowMs: 60000}), 
+    (req, res) => {
+        res.json({
+            message: "search route hit"
+        });
+    }
+);
 
 const PORT = 3000;
 app.listen(PORT, () => {
